bce245f02d29ad9db914c308f18a78deb9101239
gudaoxuri <i@sunisle.org>
2023-05-10 08:06:56 -0700

    Try k8s e2e test.

--- e2e/e2e-test.sh
+++ e2e/e2e-test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 apt update
-apt install libxml2-dev
-cargo install hurl
+apt install libxml2-dev curl
+curl --location --remote-name https://github.com/Orange-OpenSource/hurl/releases/download/3.0.0/hurl_3.0.0_amd64.deb
+apt update && apt install ./hurl_3.0.0_amd64.deb
 
 echo `kubectl get nodes -o wide`
 
@@ -12,7 +13,7 @@ echo "===echo test==="
 kubectl apply -f echo.yaml
 kubectl wait --for=condition=Ready pod -l app=echo
 
-sleep 5
+sleep 1
 
 cat>echo<<EOF 
 GET http://${cluster_ip}:9000/echo/get
