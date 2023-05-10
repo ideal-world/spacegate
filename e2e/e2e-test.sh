c227bf410bce2ecb206dfd4b4abde11be22bdf1d
gudaoxuri <i@sunisle.org>
2023-05-10 07:07:05 -0700

    Try k8s e2e test.

--- e2e/e2e-test.sh
+++ e2e/e2e-test.sh
@@ -4,22 +4,18 @@ apt update
 apt install libxml2-dev
 cargo install hurl
 
-cluster_ip=`kubectl get nodes -o jsonpath={.items[0].status.addresses[?\(@.type==\"InternalIP\"\)].address}`
+cluster_ip=`kubectl get nodes -o jsonpath={.items[1].status.addresses[?\(@.type==\"InternalIP\"\)].address}`
 
 echo "===echo test==="
 kubectl apply -f echo.yaml
 kubectl wait --for=condition=Ready pod -l app=echo
 
-echo `kubectl get nodes -o wide`
-echo "http://${cluster_ip}:9000/echo/get"
-cluster_ip2=`kubectl get nodes -o jsonpath={.items[1].status.addresses[?\(@.type==\"InternalIP\"\)].address}`
-
 cat>echo<<EOF 
-GET http://${cluster_ip2}:9000/echo/get
+GET http://${cluster_ip}:9000/echo/get
 
 HTTP 200
 [Asserts]
-jsonpath "$.url" == "http://${cluster_ip2}:9000/get"
+jsonpath "$.url" == "http://${cluster_ip}:9000/get"
 EOF
 hurl --test echo -v
 
