a6f03992b444cd28b0dffc5e95e908dcbb95604c
gudaoxuri <i@sunisle.org>
2023-05-10 18:31:14 -0700

    Try k8s e2e test.

--- e2e/e2e-test.sh
+++ e2e/e2e-test.sh
@@ -8,7 +8,7 @@ echo `kubectl get nodes -o wide`
 
 cluster_ip=`kubectl get nodes -o jsonpath={.items[1].status.addresses[?\(@.type==\"InternalIP\"\)].address}`
 
-echo "===echo test==="
+echo "============echo test============"
 kubectl apply -f echo.yaml
 kubectl wait --for=condition=Ready pod -l app=echo
 sleep 5
@@ -22,8 +22,9 @@ jsonpath "$.url" == "http://${cluster_ip}:9000/get"
 EOF
 hurl --test echo -v
 
-echo "===change config==="
+echo "============change config test============"
 kubectl patch gateway gateway --type json -p='[{"op": "replace", "path": "/spec/listeners/0/port", "value": 9001}]'
+sleep 1
 
 cat>change-port<<EOF 
 GET http://${cluster_ip}:9001/echo/get
@@ -35,6 +36,7 @@ EOF
 hurl --test change-port -v
 
 kubectl patch httproute echo --type json -p='[{"op": "replace", "path": "/spec/rules/0/matches/0/path/value", "value": "/hi"}]'
+sleep 1
 
 cat>change-route<<EOF 
 GET http://${cluster_ip}:9001/echo/get
@@ -50,3 +52,23 @@ HTTP 200
 jsonpath "$.url" == "http://${cluster_ip}:9001/get"
 EOF
 hurl --test change-route -v
+
+# TODO
+echo "============[gateway]tls test============"
+echo "============[gateway]multiple listeners test============"
+echo "============[gateway]redis connction test============"
+echo "============[websocket]no backend test============"
+echo "============[websocket]basic test============"
+echo "============[httproute]hostnames test============"
+echo "============[httproute]rule match test============"
+echo "============[httproute]timeout test============"
+echo "============[httproute]backend with k8s service test============"
+echo "============[httproute]backend with native http url test============"
+echo "============[httproute]backend weight test============"
+echo "============[filter]backend level test============"
+echo "============[filter]rule level test============"
+echo "============[filter]routing level test============"
+echo "============[filter]global level test============"
+echo "============[filter]multiple levels test============"
+
+
