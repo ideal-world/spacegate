2b78762ac8ec7eabb2a1606837452a5c320810a6
RWDai <27391645+RWDai@users.noreply.github.com>
2023-08-09 00:14:27 -0700

    status plugin use cache

--- kernel/src/config/config_by_k8s.rs
+++ kernel/src/config/config_by_k8s.rs
@@ -149,12 +149,16 @@ pub async fn init(namespaces: Option<String>) -> TardisResult<Vec<(SgGateway, Ve
         let ew = watcher(http_route_api_clone, ListParams::default()).touched_objects();
         pin_mut!(ew);
         while let Some(http_route_obj) = ew.try_next().await.expect("[SG.Config] http_route watcher error") {
-            if http_route_objs_generation.get(http_route_obj.metadata.uid.as_ref().unwrap_or(&"".to_string())).unwrap_or(&0) == &http_route_obj.metadata.generation.unwrap_or(0)
-                && http_route_api.get_metadata_opt(&http_route_obj.name_any()).await.ok().is_some()
-            {
-                // ignore the original object
-                // ignore if obj is some(it's means obj is not deleted)
-                continue;
+            if http_route_objs_generation.get(http_route_obj.metadata.uid.as_ref().unwrap_or(&"".to_string())).unwrap_or(&0) == &http_route_obj.metadata.generation.unwrap_or(0) {
+                let named_http_route_api: Api<HttpRoute> = Api::namespaced(
+                    get_client().await.expect("[SG.Config] Failed to get client"),
+                    http_route_obj.namespace().as_ref().unwrap_or(&"default".to_string()),
+                );
+                if named_http_route_api.get_metadata_opt(&http_route_obj.name_any()).await.ok().is_some() {
+                    // ignore the original object
+                    // ignore if obj is some(it's means obj is not deleted)
+                    continue;
+                }
             }
             if http_route_obj.spec.inner.parent_refs.is_none() {
                 continue;
@@ -193,8 +197,12 @@ pub async fn init(namespaces: Option<String>) -> TardisResult<Vec<(SgGateway, Ve
         pin_mut!(ew);
         while let Some(filter_obj) = ew.try_next().await.unwrap_or_default() {
             if sg_filter_objs_generation.get(filter_obj.metadata.uid.as_ref().unwrap_or(&"".to_string())).unwrap_or(&0) == &filter_obj.metadata.generation.unwrap_or(0) {
+                let named_filter_api: Api<SgFilter> = Api::namespaced(
+                    get_client().await.expect("[SG.Config] Failed to get client"),
+                    filter_obj.namespace().as_ref().unwrap_or(&"default".to_string()),
+                );
                 // Do not ignore the deletion event
-                if filter_api.get_opt(&filter_obj.name_any()).await.ok().is_some() {
+                if named_filter_api.get_opt(&filter_obj.name_any()).await.ok().is_some() {
                     // ignore the original object
                     continue;
                 }
@@ -732,8 +740,8 @@ async fn get_filters_from_cdr(kind: &str, name: &str, namespace: &Option<String>
 
     if !filter_objs.is_empty() {
         let mut filter_vec = String::new();
-        filter_objs.clone().into_iter().for_each(|filter| filter_vec.push_str(&format!("Filter{{code: {},name:{}}}", filter.code, filter.name.unwrap_or("None".to_string()))));
-        log::trace!("[SG.Config.SgFilter] {namespace}.{kind}.{name} filter found: {filter_vec}");
+        filter_objs.clone().into_iter().for_each(|filter| filter_vec.push_str(&format!("Filter{{code: {},name:{}}},", filter.code, filter.name.unwrap_or("None".to_string()))));
+        log::trace!("[SG.Config.SgFilter] {namespace}.{kind}.{name} filter found: {}", filter_vec.trim_end_matches(','));
     }
 
     if filter_objs.is_empty() {
