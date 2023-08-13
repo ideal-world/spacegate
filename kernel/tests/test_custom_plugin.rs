352cb72c89c51a85e14ba40a9bbc44d773cd81ce
RWDai <27391645+RWDai@users.noreply.github.com>
2023-08-14 07:31:19 +0800

    fix test case (#15)

--- kernel/tests/test_custom_plugin.rs
+++ kernel/tests/test_custom_plugin.rs
@@ -3,6 +3,7 @@ use std::{env, time::Duration, vec};
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
+use spacegate_kernel::config::gateway_dto::SgProtocol::Https;
 use spacegate_kernel::plugins::context::SgRoutePluginContext;
 use spacegate_kernel::plugins::filters::SgPluginFilterInitDto;
 use spacegate_kernel::{
@@ -75,6 +76,7 @@ async fn test_custom_plugin() -> TardisResult<()> {
             rules: Some(vec![SgHttpRouteRule {
                 backends: Some(vec![SgBackendRef {
                     name_or_host: "postman-echo.com".to_string(),
+                    protocol: Some(Https),
                     ..Default::default()
                 }]),
                 ..Default::default()
@@ -90,6 +92,6 @@ async fn test_custom_plugin() -> TardisResult<()> {
 
     let resp = client.get::<Value>("http://localhost:8888/get?dd", Some(vec![("Authorization".to_string(), "xxxxx".to_string())])).await?;
     assert_eq!(resp.code, 200);
-    assert!(resp.body.unwrap().get("url").unwrap().as_str().unwrap().contains("http://localhost/get?dd"));
+    assert!(resp.body.unwrap().get("url").unwrap().as_str().unwrap().contains("https://localhost/get?dd"));
     Ok(())
 }
