99b8476fdeba88cc69c538cdda8e26f66393c8fc
RWDai <27391645+RWDai@users.noreply.github.com>
2023-08-30 09:20:31 +0800

    fix test_websocket

--- kernel/tests/test_websocket.rs
+++ kernel/tests/test_websocket.rs
@@ -32,9 +32,10 @@ use tardis::{
     },
     TardisFuns,
 };
+use tardis::web::ws_processor::TardisWebsocketMgrMessage;
 
 lazy_static! {
-    static ref SENDERS: Arc<RwLock<HashMap<String, Sender<String>>>> = Arc::new(RwLock::new(HashMap::new()));
+    static ref SENDERS: Arc<RwLock<HashMap<String, Sender<TardisWebsocketMgrMessage>>>> = Arc::new(RwLock::new(HashMap::new()));
 }
 
 #[tokio::test]
@@ -291,7 +292,7 @@ impl WsApi {
     #[oai(path = "/ws/broadcast/:group/:name", method = "get")]
     async fn ws_broadcast(&self, group: Path<String>, name: Path<String>, websocket: WebSocket) -> BoxWebSocketUpgraded {
         if !SENDERS.read().await.contains_key(&group.0) {
-            SENDERS.write().await.insert(group.0.clone(), tokio::sync::broadcast::channel::<String>(100).0);
+            SENDERS.write().await.insert(group.0.clone(), tokio::sync::broadcast::channel::<TardisWebsocketMgrMessage>(100).0);
         }
         let sender = SENDERS.read().await.get(&group.0).unwrap().clone();
         if group.0 == "g1" {
@@ -314,7 +315,7 @@ impl WsApi {
                     })
                 },
                 |_, _| async move {},
-            )
+            ).await
         } else if group.0 == "g2" {
             ws_broadcast(
                 vec![name.0],
@@ -335,7 +336,7 @@ impl WsApi {
                     })
                 },
                 |_, _| async move {},
-            )
+            ).await
         } else if group.0 == "gerror" {
             ws_broadcast(
                 vec![name.0],
@@ -349,7 +350,7 @@ impl WsApi {
                     None
                 },
                 |_, _| async move {},
-            )
+            ).await
         } else {
             ws_echo(
                 name.0,
