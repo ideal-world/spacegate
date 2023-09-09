463ce47d1aed8be04d6b9b5e723bc33d9fdc3c26
RWDai <27391645+RWDai@users.noreply.github.com>
2023-09-09 16:18:20 +0800

    ExternalService expands into External Http and External Https

--- kernel/src/constants.rs
+++ kernel/src/constants.rs
@@ -6,4 +6,5 @@ pub const GATEWAY_ANNOTATION_REDIS_URL: &str = "redis_url";
 pub const GATEWAY_ANNOTATION_LOG_LEVEL: &str = "log_level";
 pub const GATEWAY_ANNOTATION_LANGUAGE: &str = "lang";
 
-pub const BANCKEND_KIND_EXTERNAL_SERVICE: &str = "ExternalService";
+pub const BANCKEND_KIND_EXTERNAL_HTTP: &str = "ExternalHttp";
+pub const BANCKEND_KIND_EXTERNAL_HTTPS: &str = "ExternalHttps";
