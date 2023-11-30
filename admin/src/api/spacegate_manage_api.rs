596694096bc98156ef0786ea296ff0517a92df66
RWDai <27391645+RWDai@users.noreply.github.com>
2023-11-30 10:03:01 +0800

    update

--- admin/src/api/spacegate_manage_api.rs
+++ admin/src/api/spacegate_manage_api.rs
@@ -1,6 +1,8 @@
 use crate::model::query_dto::{SpacegateInstQueryDto, ToInstance};
 use crate::model::vo::spacegate_inst_vo::InstConfigVo;
 use crate::service::spacegate_manage_service::SpacegateManageService;
+use kernel_common::client::k8s_client::DEFAULT_CLIENT_NAME;
+use tardis::basic::error::TardisError;
 use tardis::web::poem::session::Session;
 use tardis::web::poem_openapi;
 use tardis::web::poem_openapi::param::Query;
@@ -15,10 +17,28 @@ pub struct SpacegateManageApi;
 
 #[poem_openapi::OpenApi(prefix_path = "/spacegate")]
 impl SpacegateSelectApi {
+    /// Get select Spacegate Inst
+    #[oai(path = "/", method = "get")]
+    async fn get(&self, session: &Session) -> TardisApiResult<String> {
+        let client_name = &super::get_client_name(session).await;
+        if !client_name.is_empty() && client_name != DEFAULT_CLIENT_NAME {
+            if SpacegateManageService::check(client_name).await.is_ok() {
+                return TardisResp::ok(client_name.to_string());
+            }
+        }
+        session.set("client_name", DEFAULT_CLIENT_NAME);
+        TardisResp::ok(DEFAULT_CLIENT_NAME.to_string())
+    }
+
     /// Select Spacegate Inst
     #[oai(path = "/", method = "post")]
-    async fn add(&self, name: Query<String>, session: &Session) -> TardisApiResult<Void> {
-        SpacegateManageService::check(&name.0).await?;
+    async fn select(&self, name: Query<String>, session: &Session) -> TardisApiResult<Void> {
+        if name.0.is_empty() {
+            return TardisResp::err(TardisError::bad_request("[admin] select name cannot be empty", ""));
+        }
+        if name.0 != DEFAULT_CLIENT_NAME {
+            SpacegateManageService::check(&name.0).await?;
+        }
         session.set("client_name", &name.0);
         TardisResp::ok(Void {})
     }
