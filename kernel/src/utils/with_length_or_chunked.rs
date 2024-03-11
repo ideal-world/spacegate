4f46222cf2a390b93d5e5acccf18c7ad3738b0c2
RWDai <27391645+RWDai@users.noreply.github.com>
2024-03-11 17:11:42 +0800

    fix(test): fix redis test

--- kernel/src/utils/with_length_or_chunked.rs
+++ kernel/src/utils/with_length_or_chunked.rs
@@ -2,6 +2,8 @@ use crate::SgBody;
 use hyper::{header::HeaderValue, Response};
 
 pub fn with_length_or_chunked(resp: &mut Response<SgBody>) {
+    resp.headers_mut().remove(hyper::header::CONTENT_LENGTH);
+
     let is_chunked = resp.headers().get_all(hyper::header::TRANSFER_ENCODING).iter().any(|v| v.as_bytes() == b"chunked");
     if let Some(len) = resp.body().get_dumped().map(hyper::body::Bytes::len) {
         if !is_chunked {
--- shell/tests/test_config_by_redis.rs
+++ shell/tests/test_config_by_redis.rs
@@ -1,23 +1,24 @@
-use std::{env, time::Duration};
+use std::{collections::HashMap, env};
 
 mod init_cache_container;
+use hyper::HeaderMap;
 use serde_json::Value;
 use spacegate_kernel::BoxError;
 use tardis::{
-    basic::result::TardisResult,
     cache::cache_client::TardisCacheClient,
     config::config_dto::{CacheModuleConfig, WebClientModuleConfig},
     testcontainers,
     tokio::{
         self,
-        time::{sleep, timeout},
+        runtime::Builder,
+        time::{sleep, timeout, Duration},
     },
     web::web_client::TardisWebClient,
 };
 
 #[tokio::test]
 async fn test_config_by_redis() -> Result<(), BoxError> {
-    env::set_var("RUST_LOG", "info,spacegate_shell=trace,spacegate_kernel=trace");
+    env::set_var("RUST_LOG", "trace,spacegate_shell=trace,spacegate_kernel=trace");
     tracing_subscriber::fmt::init();
     let http_client = TardisWebClient::init(&WebClientModuleConfig {
         connect_timeout_sec: 1,
@@ -25,8 +26,6 @@ async fn test_config_by_redis() -> Result<(), BoxError> {
     })?;
     let docker = testcontainers::clients::Cli::default();
     let (cache_url, _x) = init_cache_container::init(&docker).await?;
-    // Without keys
-    assert!(spacegate_shell::startup_cache(cache_url.clone(), 1).await.is_err());
 
     let cache_client = TardisCacheClient::init(&CacheModuleConfig {
         url: cache_url.parse().expect("invalid url"),
@@ -39,20 +38,21 @@ async fn test_config_by_redis() -> Result<(), BoxError> {
             &format!(
                 r#"{{
             "name":"test_gw",
-            "listeners":[{{"port":8888,"protocol":"http"}}],
+            "listeners":[{{"port":8888,"protocol":{{"type":"http"}}}}],
             "parameters":{{"redis_url":"{cache_url}"}}
         }}"#
             ),
         )
         .await?;
     cache_client
-        .lpush(
+        .hset(
             "sg:conf:route:http:test_gw",
+            "postman-test",
             r#"{
             "gateway_name":"test_gw",
             "rules":[{
                 "backends":[{
-                    "name_or_host":"postman-echo.com",
+                    "host": {"kind":"Host","host":"postman-echo.com"},
                     "protocol":"https",
                     "port":443
                 }]
@@ -62,10 +62,23 @@ async fn test_config_by_redis() -> Result<(), BoxError> {
         .await?;
 
     // With cache url
-    spacegate_shell::startup_cache(cache_url.clone(), 1).await?;
-    sleep(Duration::from_millis(500)).await;
+    let moved_cache_url = cache_url.clone();
 
-    let resp = http_client.get::<Value>("http://localhost:8888/get?dd1", None).await?;
+    let rt = Builder::new_current_thread().enable_all().build().unwrap();
+
+    std::thread::spawn(move || {
+        let local_set = tokio::task::LocalSet::new();
+        local_set.spawn_local(async move {
+            let join_handle = spacegate_shell::startup_cache(&moved_cache_url, 1).await.unwrap();
+            join_handle.await.expect("join handle error")
+        });
+
+        rt.block_on(local_set);
+    });
+
+    sleep(Duration::from_secs(5)).await;
+
+    let resp = http_client.get::<Value>("https://localhost/get?dd1", None).await?;
     let resp = resp.body.unwrap();
     println!("resp: {:?}", resp);
     assert!(resp.get("url").unwrap().as_str().unwrap().contains("https://localhost/get?dd1"));
@@ -78,15 +91,15 @@ async fn test_config_by_redis() -> Result<(), BoxError> {
             &format!(
                 r#"{{
             "name":"test_gw",
-            "listeners":[{{"port":8889,"protocol":"http"}}],
+            "listeners":[{{"port":8889,"protocol":{{"type":"http"}}}}],
             "parameters":{{"redis_url":"{cache_url}"}}
         }}"#
             ),
         )
         .await?;
-    cache_client.set_ex("sg:conf:change:trigger:111##gateway##test_gw", "", 1).await?;
-    cache_client.set_ex("sg:conf:change:trigger:111##gateway##test_gw", "", 1).await?;
-    cache_client.set_ex("sg:conf:change:trigger:111##gateway##test_gw", "", 1).await?;
+    cache_client.set_ex("sg:conf:change:trigger:111##gateway##update##test_gw", "", 1).await?;
+    cache_client.set_ex("sg:conf:change:trigger:111##gateway##update##test_gw", "", 1).await?;
+    cache_client.set_ex("sg:conf:change:trigger:111##gateway##update##test_gw", "", 1).await?;
 
     sleep(Duration::from_millis(1500)).await;
     let resp = http_client.get::<Value>("http://localhost:8889/get?dd2", None).await?;
@@ -95,8 +108,9 @@ async fn test_config_by_redis() -> Result<(), BoxError> {
 
     // Modify route
     cache_client
-        .lpush(
+        .hset(
             "sg:conf:route:http:test_gw",
+            "postman-test",
             r#"{
             "gateway_name":"test_gw",
             "rules":[{
@@ -109,9 +123,9 @@ async fn test_config_by_redis() -> Result<(), BoxError> {
         }"#,
         )
         .await?;
-    cache_client.set_ex("sg:conf:change:trigger:222##httproute##test_gw", "", 1).await?;
-    cache_client.set_ex("sg:conf:change:trigger:222##httproute##test_gw", "", 1).await?;
-    cache_client.set_ex("sg:conf:change:trigger:222##httproute##test_gw", "", 1).await?;
+    cache_client.set_ex("sg:conf:change:trigger:222##httproute##update##test_gw##postman-test", "", 1).await?;
+    cache_client.set_ex("sg:conf:change:trigger:222##httproute##update##test_gw##postman-test", "", 1).await?;
+    cache_client.set_ex("sg:conf:change:trigger:222##httproute##update##test_gw##postman-test", "", 1).await?;
 
     sleep(Duration::from_millis(1500)).await;
     let resp = http_client.get::<Value>("http://localhost:8889/get?dd3", None).await?;
@@ -120,7 +134,7 @@ async fn test_config_by_redis() -> Result<(), BoxError> {
 
     // Remove gateway
     cache_client.hdel("sg:conf:gateway", "test_gw").await?;
-    cache_client.set_ex("sg:conf:change:trigger:333##gateway##test_gw", "", 1).await?;
+    cache_client.set_ex("sg:conf:change:trigger:333##gateway##delete##test_gw", "", 1).await?;
 
     sleep(Duration::from_millis(1500)).await;
     let resp = timeout(Duration::from_secs(1), http_client.get_to_str("http://localhost:8889/get?dd4", None)).await;
