df347f4f3e94e1ad14cdb6665cd1acd48d1ee9e3
4t145 <u4t145@163.com>
2024-03-11 14:51:48 +0800

    fix client method

--- admin-client/src/api/index.ts
+++ admin-client/src/api/index.ts
@@ -161,9 +161,9 @@ export async function delete_config_item_all_routes(gatewayName: string,): Promi
 **********************************************/
 
 export async function pluginList(): Promise<AxiosResponse<Array<string>>> {
-    return Client.axiosInstance.delete(`/plugin/list`)
+    return Client.axiosInstance.get(`/plugin/list`)
 }
 
 export async function pluginSchema(code: string): Promise<AxiosResponse<unknown>> {
-    return Client.axiosInstance.delete(`/plugin/schema/${code}`)
+    return Client.axiosInstance.get(`/plugin/schema/${code}`)
 }
