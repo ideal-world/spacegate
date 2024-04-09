e5e97fcc85abdab64d09b42fcc6bacdfb0d149fe
4t145 <u4t145@163.com>
2024-04-09 11:25:02 +0800

    modify plugin layers load order

--- kernel/src/utils/fold_sg_layers.rs
+++ kernel/src/utils/fold_sg_layers.rs
@@ -2,8 +2,8 @@ use tower_layer::Layer;
 
 use crate::{ArcHyperService, SgBoxLayer};
 
-pub fn sg_layers<'a>(layers: impl Iterator<Item = &'a SgBoxLayer>, mut inner: ArcHyperService) -> ArcHyperService {
-    for l in layers {
+pub fn sg_layers<'a>(layers: impl Iterator<Item = &'a SgBoxLayer> + DoubleEndedIterator, mut inner: ArcHyperService) -> ArcHyperService {
+    for l in layers.rev() {
         inner = l.layer(inner);
     }
     inner
