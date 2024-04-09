491a7c89280108ad0400fe86987faf4ae8ea2def
4t145 <u4t145@163.com>
2024-04-09 18:36:10 +0800

    update

--- crates/admin-server/src/service/plugin.rs
+++ crates/admin-server/src/service/plugin.rs
@@ -15,11 +15,11 @@ use tokio::{sync::RwLock, time::Instant};
 
 static ATTR_CACHE: OnceLock<RwLock<HashMap<String, PluginAttributes>>> = OnceLock::new();
 
-async fn sync_attr_cache<B: Discovery>(backend: &B) -> Result<(), BoxError> {
+async fn sync_attr_cache<B: Discovery>(backend: &B, refresh: bool) -> Result<(), BoxError> {
     static NEXT_SYNC: OnceLock<RwLock<Instant>> = OnceLock::new();
-    const SYNC_INTERVAL: std::time::Duration = std::time::Duration::from_secs(60);
+    const SYNC_INTERVAL: std::time::Duration = std::time::Duration::from_secs(3600);
     let next_sync = NEXT_SYNC.get_or_init(|| RwLock::new(Instant::now())).read().await;
-    if next_sync.elapsed() != Duration::ZERO {
+    if next_sync.elapsed() != Duration::ZERO || refresh {
         drop(next_sync);
         let mut cache = ATTR_CACHE.get_or_init(Default::default).write().await;
         if let Some(remote) = backend.api_url().await? {
@@ -49,15 +49,15 @@ async fn get_schema_by_code<B: Discovery>(Path(plugin_code): Path<String>, State
     }
 }
 async fn get_list<B: Discovery>(State(AppState { backend, .. }): State<AppState<B>>) -> Result<Json<Vec<String>>, InternalError> {
-    sync_attr_cache(backend.as_ref()).await.map_err(InternalError)?;
+    sync_attr_cache(backend.as_ref(), false).await.map_err(InternalError)?;
     Ok(Json(ATTR_CACHE.get_or_init(Default::default).read().await.keys().cloned().collect()))
 }
 async fn get_attr_all<B: Discovery>(State(AppState { backend, .. }): State<AppState<B>>) -> Result<Json<Vec<PluginAttributes>>, InternalError> {
-    sync_attr_cache(backend.as_ref()).await.map_err(InternalError)?;
+    sync_attr_cache(backend.as_ref(), false).await.map_err(InternalError)?;
     Ok(Json(ATTR_CACHE.get_or_init(Default::default).read().await.values().cloned().collect()))
 }
 async fn get_attr<B: Discovery>(Path(plugin_code): Path<String>, State(AppState { backend, .. }): State<AppState<B>>) -> Result<Json<Option<PluginAttributes>>, InternalError> {
-    sync_attr_cache(backend.as_ref()).await.map_err(InternalError)?;
+    sync_attr_cache(backend.as_ref(), false).await.map_err(InternalError)?;
     Ok(Json(ATTR_CACHE.get_or_init(Default::default).read().await.get(&plugin_code).cloned()))
 }
 
@@ -69,5 +69,5 @@ where
         .route("/schema/:plugin_code", get(get_schema_by_code::<B>))
         .route("/list", get(get_list::<B>))
         .route("/attr-all", get(get_attr_all::<B>))
-        .route("/attr", get(get_attr::<B>))
+        .route("/attr/:plugin_code", get(get_attr::<B>))
 }
