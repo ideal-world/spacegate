208e556550c1d3a99b89874e89646d3df55bdab1
4t145 <u4t145@163.com>
2024-05-13 17:25:26 +0800

    add logs

--- crates/plugin/src/lib.rs
+++ crates/plugin/src/lib.rs
@@ -33,7 +33,7 @@ pub mod plugins;
 #[cfg(feature = "schema")]
 pub use schemars;
 pub use spacegate_model::{plugin_meta, PluginAttributes, PluginConfig, PluginInstanceId, PluginInstanceMap, PluginInstanceName, PluginMetaData};
-
+pub use spacegate_model;
 /// # Plugin Trait
 /// It's a easy way to define a plugin through this trait.
 /// You should give a unique [`code`](Plugin::CODE) for the plugin,
