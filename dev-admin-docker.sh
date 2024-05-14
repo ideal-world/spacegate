28c45d676e9b6bf73bb3393cf5684b641a757260
4t145 <u4t145@163.com>
2024-05-14 16:43:46 +0800

    remove docker script

--- dev-admin-docker.sh
+++
@@ -1,5 +0,0 @@
-cargo build -p spacegate-admin-server
-mv ./target/debug/spacegate-admin-server ./resource/docker/spacegate-admin-server
-cd ./resource/docker/spacegate-admin-server
-docker build -t 172.30.84.225:443/idp-prod/spacegate-admin-server:v0.2.4  .
-docker push 172.30.84.225:443/idp-prod/spacegate-admin-server:v0.2.4
