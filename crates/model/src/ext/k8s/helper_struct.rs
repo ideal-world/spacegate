6298350303bd2538464b5a094267eeda6ba44a19
RWDai <27391645+RWDai@users.noreply.github.com>
2024-05-17 06:38:23 +0000

    feat: instance format support namespace

--- crates/model/src/ext/k8s/helper_struct.rs
+++ crates/model/src/ext/k8s/helper_struct.rs
@@ -6,31 +6,6 @@ pub struct SgSingeFilter {
     pub target_ref: Option<super::crd::sg_filter::K8sSgFilterSpecTargetRef>,
 }
 
-//TODO remove
-// impl PartialEq for SgSingeFilter {
-//     fn eq(&self, other: &Self) -> bool {
-//         self.name == other.name
-//             && self.namespace == other.namespace
-//             && self.filter.code == other.filter.code
-//             && self.target_ref.kind == other.target_ref.kind
-//             && self.target_ref.name == other.target_ref.name
-//             && self.target_ref.namespace.as_ref().unwrap_or(&constants::DEFAULT_NAMESPACE.to_string())
-//                 == other.target_ref.namespace.as_ref().unwrap_or(&constants::DEFAULT_NAMESPACE.to_string())
-//     }
-// }
-
-// impl Eq for SgSingeFilter {}
-
-// impl Hash for SgSingeFilter {
-//     fn hash<H: Hasher>(&self, state: &mut H) {
-//         self.name.hash(state);
-//         self.namespace.hash(state);
-//         self.target_ref.kind.hash(state);
-//         self.target_ref.name.hash(state);
-//         self.target_ref.namespace.hash(state);
-//     }
-// }
-
 impl From<SgSingeFilter> for super::crd::sg_filter::SgFilter {
     fn from(value: SgSingeFilter) -> Self {
         super::crd::sg_filter::SgFilter {
