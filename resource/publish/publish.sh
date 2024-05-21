245b86e4812d152224f45717c476aa14fe542388
4t145 <u4t145@163.com>
2024-05-21 16:00:07 +0800

    remove path dependencies, skip published crates

---
+++ resource/publish/publish.sh
@@ -0,0 +1,10 @@
+VERSION=$1
+PACKAGE_NAME=$2
+echo "Publishing version $VERSION of $PACKAGE_NAME";
+cargo search "$PACKAGE_NAME" --registry crates-io | grep -q "$VERSION"
+if [ $? -eq 0 ]; then
+    echo "Version $VERSION of $PACKAGE_NAME is already published."
+else
+    cargo package -p $PACKAGE_NAME 
+    cargo publish -p $PACKAGE_NAME --registry crates-io
+fi
