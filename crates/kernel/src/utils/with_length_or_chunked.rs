ef6d5bde051b88373d3ddeadb6eac4c19bb2a016
RWDai <27391645+RWDai@users.noreply.github.com>
2024-10-10 10:57:03 +0800

    chore: cargo fmt

--- crates/kernel/src/utils/with_length_or_chunked.rs
+++ crates/kernel/src/utils/with_length_or_chunked.rs
@@ -4,17 +4,17 @@ use hyper::{header::HeaderValue, Request, Response};
 #[allow(dead_code)]
 /// Set the content length header or transfer encoding to chunked for Request.
 pub fn req_length_or_chunked(req: &mut Request<SgBody>) {
-  const CHUNKED: &str = "chunked";
-  req.headers_mut().remove(hyper::header::CONTENT_LENGTH);
-  if let Some(len) = req.body().get_dumped().map(hyper::body::Bytes::len) {
-    req.headers_mut().remove(hyper::header::TRANSFER_ENCODING);
-    req.headers_mut().insert(
-          hyper::header::CONTENT_LENGTH,
-          HeaderValue::from_str(len.to_string().as_str()).expect("digits should be valid header char"),
-      );
-  } else if !req.headers().get_all(hyper::header::TRANSFER_ENCODING).iter().any(|v| v.as_bytes() == CHUNKED.as_bytes()) {
-    req.headers_mut().append(hyper::header::TRANSFER_ENCODING, HeaderValue::from_static(CHUNKED));
-  }
+    const CHUNKED: &str = "chunked";
+    req.headers_mut().remove(hyper::header::CONTENT_LENGTH);
+    if let Some(len) = req.body().get_dumped().map(hyper::body::Bytes::len) {
+        req.headers_mut().remove(hyper::header::TRANSFER_ENCODING);
+        req.headers_mut().insert(
+            hyper::header::CONTENT_LENGTH,
+            HeaderValue::from_str(len.to_string().as_str()).expect("digits should be valid header char"),
+        );
+    } else if !req.headers().get_all(hyper::header::TRANSFER_ENCODING).iter().any(|v| v.as_bytes() == CHUNKED.as_bytes()) {
+        req.headers_mut().append(hyper::header::TRANSFER_ENCODING, HeaderValue::from_static(CHUNKED));
+    }
 }
 
 /// Set the content length header or transfer encoding to chunked for Response.
