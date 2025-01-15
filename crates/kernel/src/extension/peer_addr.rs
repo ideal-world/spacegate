106bbef203665be0479e135515eedf48568f7f76
4t145 <u4t145@163.com>
2025-01-15 14:02:45 +0800

    add extraction for peer-addr

--- crates/kernel/src/extension/peer_addr.rs
+++ crates/kernel/src/extension/peer_addr.rs
@@ -1,4 +1,13 @@
 use std::net::SocketAddr;
+
+use crate::Extract;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 #[repr(transparent)]
 pub struct PeerAddr(pub SocketAddr);
+
+impl Extract for PeerAddr {
+    fn extract(req: &hyper::Request<crate::SgBody>) -> Self {
+        let peer_addr = req.extensions().get::<PeerAddr>().expect("PeerAddr not found");
+        *peer_addr
+    }
+}
