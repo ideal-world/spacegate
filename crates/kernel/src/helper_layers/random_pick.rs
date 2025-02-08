4a7a9eb8241593e9fc969cc59fa78f10b98c61a6
RWDai <27391645+RWDai@users.noreply.github.com>
2025-02-08 14:27:37 +0800

    chore: update rand dependency and adjust import paths

--- crates/kernel/src/helper_layers/random_pick.rs
+++ crates/kernel/src/helper_layers/random_pick.rs
@@ -1,19 +1,19 @@
 use std::sync::Arc;
 
-use rand::distributions::Distribution;
+use rand::distr::Distribution;
 
 #[derive(Clone)]
 pub struct RandomPick<I, S>
 where
-    I: rand::distributions::uniform::SampleUniform + std::cmp::PartialOrd,
+    I: rand::distr::uniform::SampleUniform + std::cmp::PartialOrd,
 {
-    picker: Arc<rand::distributions::WeightedIndex<I>>,
+    picker: Arc<rand::distr::weighted::WeightedIndex<I>>,
     services: Arc<[S]>,
 }
 
 impl<I, S> std::fmt::Debug for RandomPick<I, S>
 where
-    I: rand::distributions::uniform::SampleUniform + std::cmp::PartialOrd,
+    I: rand::distr::uniform::SampleUniform + std::cmp::PartialOrd,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("RandomPick").finish()
@@ -22,13 +22,13 @@ where
 
 impl<I, S> RandomPick<I, S>
 where
-    I: rand::distributions::uniform::SampleUniform + std::cmp::PartialOrd + Clone + Default + for<'a> std::ops::AddAssign<&'a I>,
+    I: rand::distr::uniform::SampleUniform + std::cmp::PartialOrd + Clone + Default + for<'a> std::ops::AddAssign<&'a I> + rand::distr::weighted::Weight,
 {
     pub fn new(services: impl IntoIterator<Item = (I, S)>) -> Self {
         let (weights, services): (Vec<_>, Vec<_>) = services.into_iter().unzip();
         assert!(!services.is_empty(), "services must not be empty");
         Self {
-            picker: Arc::new(rand::distributions::WeightedIndex::new(weights).expect("invalid weights")),
+            picker: Arc::new(rand::distr::weighted::WeightedIndex::new(weights).expect("invalid weights")),
             services: services.into(),
         }
     }
@@ -38,7 +38,7 @@ impl<I, R, S> hyper::service::Service<R> for RandomPick<I, S>
 where
     S: hyper::service::Service<R>,
     S::Future: std::marker::Send,
-    I: rand::distributions::uniform::SampleUniform + std::cmp::PartialOrd + Clone,
+    I: rand::distr::uniform::SampleUniform + std::cmp::PartialOrd + Clone,
 {
     type Response = S::Response;
     type Error = S::Error;
