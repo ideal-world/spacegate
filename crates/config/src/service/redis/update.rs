90bc2b66f8da9d8953966c80013d433d592cd889
4t145 <u4t145@163.com>
2025-02-10 11:26:30 +0800

    fix: avoid Redis never type convert error, fix build warnings

--- crates/config/src/service/redis/update.rs
+++ crates/config/src/service/redis/update.rs
@@ -10,17 +10,17 @@ where
     F: ConfigFormat + Send + Sync,
 {
     async fn update_config_item_gateway(&self, gateway_name: &str, gateway: crate::model::SgGateway) -> BoxResult<()> {
-        self.get_con().await?.hset(CONF_GATEWAY_KEY, gateway_name, self.format.ser(&gateway)?).await?;
+        let _: () = self.get_con().await?.hset(CONF_GATEWAY_KEY, gateway_name, self.format.ser(&gateway)?).await?;
         let event = RedisConfEvent(
             crate::service::ConfigType::Gateway { name: gateway_name.to_string() },
             crate::service::ConfigEventType::Update,
         );
-        self.get_con().await?.publish(CONF_EVENT_CHANNEL, event).await?;
+        let _: () = self.get_con().await?.publish(CONF_EVENT_CHANNEL, event).await?;
         Ok(())
     }
 
     async fn update_config_item_route(&self, gateway_name: &str, route_name: &str, route: crate::model::SgHttpRoute) -> BoxResult<()> {
-        self.get_con().await?.hset(format!("{}{}", CONF_HTTP_ROUTE_KEY, gateway_name), route_name, self.format.ser(&route)?).await?;
+        let _: () = self.get_con().await?.hset(format!("{}{}", CONF_HTTP_ROUTE_KEY, gateway_name), route_name, self.format.ser(&route)?).await?;
         let event = RedisConfEvent(
             crate::service::ConfigType::Route {
                 gateway_name: gateway_name.to_string(),
@@ -28,14 +28,14 @@ where
             },
             crate::service::ConfigEventType::Update,
         );
-        self.get_con().await?.publish(CONF_EVENT_CHANNEL, event).await?;
+        let _: () = self.get_con().await?.publish(CONF_EVENT_CHANNEL, event).await?;
         Ok(())
     }
 
     async fn update_plugin(&self, id: &crate::model::PluginInstanceId, value: serde_json::Value) -> BoxResult<()> {
-        self.get_con().await?.hset(CONF_PLUGIN_KEY, id.to_string(), value.to_string()).await?;
+        let _: () = self.get_con().await?.hset(CONF_PLUGIN_KEY, id.to_string(), value.to_string()).await?;
         let event = RedisConfEvent(crate::service::ConfigType::Plugin { id: id.clone() }, crate::service::ConfigEventType::Update);
-        self.get_con().await?.publish(CONF_EVENT_CHANNEL, event).await?;
+        let _: () = self.get_con().await?.publish(CONF_EVENT_CHANNEL, event).await?;
         Ok(())
     }
 }
